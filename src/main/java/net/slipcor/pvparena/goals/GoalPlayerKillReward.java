cc41442029c10cab723a6788b18b3fc934b564d7
Eredrim <Eredrim@users.noreply.github.com>
2022-05-19 23:44:39 +0200

    fix: deserialization in PlayerKillReward and global classes (recovered from 1.15.3)

--- src/main/java/net/slipcor/pvparena/goals/GoalPlayerKillReward.java
+++ src/main/java/net/slipcor/pvparena/goals/GoalPlayerKillReward.java
@@ -38,6 +38,7 @@ import java.util.Map;
 import java.util.Set;
 
 import static net.slipcor.pvparena.config.Debugger.debug;
+import static net.slipcor.pvparena.core.ItemStackUtils.getItemStacksFromConfig;
 import static net.slipcor.pvparena.core.Utils.getSerializableItemStacks;
 
 /**
@@ -319,16 +320,18 @@ public class GoalPlayerKillReward extends ArenaGoal {
 
     @Override
     public void setDefaults(final YamlConfiguration config) {
-        final ConfigurationSection cs = (ConfigurationSection) config.get("goal.playerkillrewards");
+        ConfigurationSection cs = (ConfigurationSection) config.get("goal.playerkillrewards");
 
         if (cs != null) {
             for (String line : cs.getKeys(false)) {
                 try {
-                    this.getItemMap().put(Integer.parseInt(line.substring(2)),
-                            new ItemStack[][]{
-                                    cs.getList(line + ".items").toArray(new ItemStack[0]),
-                                    cs.getList(line + ".offhand").toArray(new ItemStack[]{new ItemStack(Material.AIR, 1)}),
-                                    cs.getList(line + ".armor").toArray(new ItemStack[0])
+                    ConfigurationSection classesCfg = cs.getConfigurationSection(line);
+                    int classIndex = Integer.parseInt(line.substring(2));
+                    this.getItemMap().put(classIndex,
+                            new ItemStack[][] {
+                                    getItemStacksFromConfig(classesCfg.getList("items")),
+                                    getItemStacksFromConfig(classesCfg.getList("offhand")),
+                                    getItemStacksFromConfig(classesCfg.getList("armor")),
                             });
                 } catch (final Exception ignored) {
                 }
